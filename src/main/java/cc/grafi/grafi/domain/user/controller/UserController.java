3102e21fea6969d565236c1f949a34ac8c978008
맹순영 <symaeng98@naver.com>
2023-09-09 15:46:43 +0900

    chore: swagger 추가

--- src/main/java/cc/grafi/grafi/domain/user/controller/UserController.java
+++ src/main/java/cc/grafi/grafi/domain/user/controller/UserController.java
@@ -4,6 +4,10 @@ import cc.grafi.grafi.domain.user.dto.UserCreationRequest;
 import cc.grafi.grafi.domain.user.dto.UserDetailResponse;
 import cc.grafi.grafi.domain.user.dto.UserUpdateRequest;
 import cc.grafi.grafi.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "users")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -18,18 +23,36 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "값을 누락하고 보냈거나, 값의 타입이 잘못된 경우"),
+            @ApiResponse(responseCode = "500", description = "서버 오류"),
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<UserDetailResponse> getUserById(@PathVariable("userId") String userId) {
         UserDetailResponse response = userService.getUser(userId);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "회원가입")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "값을 누락하고 보냈거나, 값의 타입이 잘못된 경우"),
+            @ApiResponse(responseCode = "500", description = "서버 오류"),
+    })
     @PostMapping()
     public ResponseEntity<String> saveUser(@RequestBody @Valid UserCreationRequest request) {
         userService.saveUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
     }
 
+    @Operation(summary = "회원 정보 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 정보 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "값을 누락하고 보냈거나, 값의 타입이 잘못된 경우"),
+            @ApiResponse(responseCode = "500", description = "서버 오류"),
+    })
     @PutMapping()
     public ResponseEntity<String> updateUser(@RequestBody @Valid UserUpdateRequest request) {
         userService.updateUser(request);
